a89111afe804c096c2b358712e610a946fe1d242
noecl <noecl1@hotmail.com>
2019-03-25 22:26:09 -0700

    Removed unnecesary lines

--- vlsm.py
+++ vlsm.py
@@ -6,8 +6,6 @@ from prettytable import PrettyTable
 from colorama import init, Fore, Style, Back
 import pprint
 
-#class IPNetwork(BaseIP, IPListMixin):
-
 def clearTerminal():
     if os.name == 'nt':
         os.system('cls')
@@ -16,8 +14,6 @@ def clearTerminal():
         os.system('clear')
         pass
     pass
-
-# 'nt' for Windows, posix' for linux
 
 def validateIP(ip):
     return ip.valid_ipv4(ip)
@@ -30,27 +26,6 @@ def printHeader():
     print("             by Noé Camacho Lizárraga           ")
     print("                                               ", Style.RESET_ALL, '\n\n')
 
-def test(ip):
-        print("ITER HOSTS:", ip.iter_hosts)
-        print("IP LAST:",ip.last)
-        print("IP NEXT:",ip.next(1))
-        # print(ip.netmask.bits())
-        print("BITS USADOS RED",ip.netmask.bits())
-        print("BROADCAST", ip.broadcast.bits())
-        print("BITS DE RED", ip.prefixlen/8)
-        print("IP_ADD:",ip.__iadd__(1))
-        network = ip.netmask.bits()
-        print("SUPERNET:",ip.subnet(ip.prefixlen))
-        
-
-        stringtemp = ip.__str__()
-        word = []
-        word.append(stringtemp.split('.'))
-        print("STRING;",word)
-        
-        
-        pass
-
 def printTable(table, majorNet, neededHosts, allocatedHosts):
     clearTerminal()
     ipTemp = IPNetwork(majorNet)
@@ -60,7 +35,7 @@ def printTable(table, majorNet, neededHosts, allocatedHosts):
     print("         | | | |  SUBNETEO EXITOSO | | | |      ")
     print("                                               ", Style.RESET_ALL, '\n\n')
     print(Back.WHITE, Fore.BLACK,Style.NORMAL,Style.NORMAL,' RED PRINCIPAL:', Fore.WHITE, Back.GREEN, Style.BRIGHT,'{}'.format(ipTemp),Style.RESET_ALL)
-    print(Back.WHITE, Fore.BLACK,Style.NORMAL,' Redes Disponibles en RED PRINCIPAL:',Fore.WHITE,Back.GREEN,Style.BRIGHT,'{}'.format(ipTemp.size - 2),Style.RESET_ALL)
+    print(Back.WHITE, Fore.BLACK,Style.NORMAL,' Redes Disponibles en RED PRINCIPAL:',Fore.WHITE,Back.GREEN,Style.BRIGHT,'{}'.format(ipTemp.size),Style.RESET_ALL)
     print(Back.WHITE, Fore.BLACK,Style.NORMAL,' Numero de direcciones IP necesarias:',Fore.WHITE, Back.GREEN,Style.BRIGHT,'{}'.format(neededHosts),Style.RESET_ALL)
     print(Back.WHITE, Fore.BLACK,Style.NORMAL,' Direcciones IP almacenadas en las subredes:',Fore.WHITE,Back.GREEN,Style.BRIGHT,'{}'.format(allocatedHosts),Style.RESET_ALL)
     print(Back.WHITE, Fore.BLACK,Style.NORMAL,' Cerca del',Fore.WHITE,Back.GREEN,Style.BRIGHT,'{}%'.format(int((allocatedHosts*100)/ipTemp.size-2)),Back.WHITE,Fore.BLACK,Style.NORMAL,'de espacio de la RED PRINCIPAL es usado',Style.RESET_ALL)    
@@ -68,14 +43,11 @@ def printTable(table, majorNet, neededHosts, allocatedHosts):
     print("\n\n")
     print(table)
 
-
 def requestNets(netSize):
     hostSize = {}
     print('\n\n|  RED  | No. Hosts |')
     for hosts in range(0, netSize):
         hostSize[hosts+1] = (int(input('>   {}        '.format(hosts+1))) + 2)
-    # for k, v in hostSize.items():
-    #     print(k, v)
     return hostSize
 
 def calculateNetwork(ip, hostSize, pos):
@@ -86,23 +58,16 @@ def calculateNetwork(ip, hostSize, pos):
     del(temp3[1]) 
     temp3.append(str(cidr))
     chara= '/'
-    # print('TEMP3',temp3)
-    # print(len(temp3))
     ip = IPNetwork(chara.join(temp3))
-    # print("ip: ",ip)
     return ip 
 
 def calculateCidr(hostSize, pos):
     for num in range(0,100):
-        # print(num, 2**num)
         if (hostSize[pos] <= 2**num):
-            # print("===========>",num, 2**num)
-            # print('CIDR:', 32-num)
-            # print('Mask:', num/8)
-            # print("Host:", 32-num/8)
             return 32-num
         else:
             pass
+
 def main():
     init()
     clearTerminal()
@@ -119,7 +84,6 @@ def main():
         for pos in range(0, netSize):
             ip = calculateNetwork(ip, netDict, pos+1)
             ip_list = list(ip)
-            # test(ip)
             table.add_row([pos+1, netDict[pos+1]-2, ip.size-2, ip.ip, '{} - {}'.format(ip_list[1], ip[len(ip_list)-2]), ip.broadcast, '/{}'.format(ip.prefixlen), ip.netmask])
             neededHosts = neededHosts + (netDict[pos+1]-2)
             allocatedHosts = allocatedHosts + (ip.size-2)
@@ -133,33 +97,3 @@ def main():
     
 if __name__ == "__main__":
        main()
-
-    # print('Dirección de red(ip):', ip.ip)
-    # print('Dirección de red(network):', ip.network)
-    # print('Rango de IP: {} - {}'.format(ip_list[1], ip[len(ip_list)-2]))
-    # print('Broadcast:', ip.broadcast)
-    # print('CIDR: /',ip.prefixlen)
-    # print('CIDR2:', ip.cidr)
-    # print('Tamaño:', ip.size)
-    # print('Máscara de red:', ip.netmask)
-    # print('Máscara de host:', ip.hostmask)
-    # print('Version:', ip.version)
-    # print('Cantidad de Hosts:', len(ip_list))
-
-    #print('version:{}'.format(ip.version()))
-    # print('¿Cuántas subredes deseas crear?: ')
-    #netSize = int(input('¿Cuántas subredes deseas crear?:'))
-    #subNets = []
-    #print('## Ingresa el número de hosts para cada red')
-    #for network in range(1, netSize+1):
-        # String formatting in `input statement` instead of a print
-    #    temp = input('Red {}: '.format(network))
-    #    subNets.append(temp)
-    #print(subNets)
-
-""""
-[ X ] Request Major Network
-[ ] Calculate the CIDR
-[ ] Calculate the subnet Mask
-[ ] Calculate the jump 
-"""
